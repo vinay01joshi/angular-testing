61d281a482fb0732af294bf10c8f4854c22412b8
Vinay Joshi <vinjoshi@publicisgroupe.net>
2017-09-03 10:24:04 +0530

    Unit Testing | Working with Forms

--- src/app/04-forms/todo-form.component.spec.ts
+++ src/app/04-forms/todo-form.component.spec.ts
@@ -1,15 +1,23 @@
+import { FormBuilder } from '@angular/forms';
 import { TodoFormComponent } from './todo-form.component'; 
 
 describe('TodoFormComponent', () => {
   var component: TodoFormComponent; 
 
   beforeEach(() => {
-
+    component = new TodoFormComponent(new FormBuilder());
   });
 
-  it('', () => {
+  it('should create a from with 2 control', () => {
+    expect(component.form.contains('name')).toBeTruthy();           
+    expect(component.form.contains('email')).toBeTruthy();           
   });
 
-  it('', () => {
+  it('shoud make the name control required', () => {
+    let control = component.form.get('name');
+    
+    control.setValue('');
+
+    expect(control.valid).toBeFalsy();
   });
 });
