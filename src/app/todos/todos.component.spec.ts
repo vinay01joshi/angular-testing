0bb0d06bb2068c011a51714c6acdee1a6a3df14f
Vinay Joshi <vinjoshi@publicisgroupe.net>
2017-09-05 08:36:46 +0530

    Integration Testing | Dealing with Asynchronous Operations- Testing with promise

--- src/app/todos/todos.component.spec.ts
+++ src/app/todos/todos.component.spec.ts
@@ -24,11 +24,13 @@ describe('TodosComponent', () => {
     // fixture.detectChanges();    
   });
 
-  it('should load todos from the server', () => {
+  it('should load todos from the server', async(() => {
     let service = TestBed.get(TodoService);
-    spyOn(service,'getTodos').and.returnValue(Observable.from([ [1,2,3] ] ));
+    spyOn(service,'getTodosPromise').and.returnValue(Promise.resolve([1,2,3]));
     fixture.detectChanges();
 
-    expect(component.todos.length).toBe(3);
-  });
+    fixture.whenStable().then(()=>{
+      expect( component.todos.length).toBe(3);
+    });
+  }));
 });
